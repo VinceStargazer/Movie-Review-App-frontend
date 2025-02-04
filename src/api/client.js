1fc67bf042b5e40bce243945ddc3e0156497840e
VinceStargazer <yujw88746113@gmail.com>
2025-02-04 14:16:27 -0800

    update backend api

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // "https://movie-review-backend-qaalv.ondigitalocean.app/api"
 // "http://localhost:8000/api"
-const client = axios.create({ baseURL: "https://movie-review-app-backend-2s76.onrender.com/api"});
+const client = axios.create({ baseURL: "https://movie-review-app-backend-7yu4.onrender.com/api"});
 
 export default client;
  
